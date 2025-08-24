4e955f86391f95ca82aa0c4ac2dd639ad517565f
hindessc <hindessc@gmail.com>
2025-08-24 16:27:17 +0100

    Added strings with concatenation

--- vm.c
+++ vm.c
@@ -1,10 +1,13 @@
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "vm.h"
 #include "debug.h"
+#include "object.h"
+#include "memory.h"
 
 VM vm;
 
@@ -27,9 +30,11 @@ static void runtimeError(const char* format, ...) {
 
 void initVM() {
   resetStack();
+  vm.objects = NULL;
 }
 
 void freeVM() {
+  freeObjects();
 }
 
 void push(Value value) {
@@ -48,6 +53,21 @@ static Value peek(int distance) {
 
 static bool isFalsey(Value value) {
   return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
+static void concatenate() {
+  ObjString* b = AS_STRING(*(vm.stackTop-1));
+  ObjString* a = AS_STRING(*(vm.stackTop-2));
+
+  int length = a->length + b->length;
+  char* chars = ALLOCATE(char, length + 1);
+  memcpy(chars, a->chars, a->length);
+  memcpy(chars + a->length, b->chars, b->length);
+  chars[length] = '\0';
+
+  ObjString* result = takeString(chars, length);
+  *(vm.stackTop-2) = OBJ_VAL(result);
+  vm.stackTop--;
 }
 
 static InterpretResult run() {
@@ -96,7 +116,18 @@ static InterpretResult run() {
         break;
       case OP_GREATER:  BINARY_OP(BOOL_VAL, >); break;
       case OP_LESS:     BINARY_OP(BOOL_VAL, <); break;
-      case OP_ADD: BINARY_OP(NUMBER_VAL, +);break;
+      case OP_ADD: {
+        if (IS_STRING(peek(0)) && IS_STRING(peek(1))) {
+          concatenate();
+        } else if (IS_NUMBER(peek(0)) && IS_NUMBER(peek(1))) {
+          BINARY_OP(NUMBER_VAL, +);
+        } else {
+          runtimeError(
+            "Operands must be two numbers or two strings.");
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        break;
+      }
       case OP_SUBTRACT: BINARY_OP(NUMBER_VAL, -);break;
       case OP_MULTIPLY: BINARY_OP(NUMBER_VAL, *);break;
       case OP_DIVIDE: BINARY_OP(NUMBER_VAL, /);break;
