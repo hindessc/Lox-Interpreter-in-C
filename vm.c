a2be1e37f1bbbe01b25c5b5e4330352baf3b31a5
hindessc <hindessc@gmail.com>
2025-08-26 12:07:23 +0100

    If then else statements

--- vm.c
+++ vm.c
@@ -76,6 +76,8 @@ static void concatenate() {
 
 static InterpretResult run() {
   #define READ_BYTE()(*vm.ip++)
+  #define READ_SHORT() \
+    (vm.ip += 2, (uint16_t)((vm.ip[-2] << 8) | vm.ip[-1]))
   #define READ_CONSTANT()(vm.chunk->constants.values[READ_BYTE()])
   #define READ_STRING() AS_STRING(READ_CONSTANT())
 
@@ -88,10 +90,6 @@ static InterpretResult run() {
       *(vm.stackTop-2) = valueType(AS_NUMBER(*(vm.stackTop-2)) op AS_NUMBER(*(vm.stackTop-1)));\
       vm.stackTop--;\
     } while (false)
-
-  for (int i = 0; i < vm.chunk->count; i++) {
-    printf("%d ", vm.chunk->code[i]);
-  }
 
   for (;;) {
 
@@ -195,6 +193,16 @@ static InterpretResult run() {
         printf("\n");
         break;
       }
+      case OP_JUMP: {
+        uint16_t offset = READ_SHORT();
+        vm.ip += offset;
+        break;
+      }
+      case OP_JUMP_IF_FALSE: {
+        uint16_t offset = READ_SHORT();
+        if (isFalsey(peek(0))) vm.ip += offset;
+        break;
+      }
       case OP_RETURN: {
         // Exit interpreter.
         return INTERPRET_OK;
@@ -204,6 +212,7 @@ static InterpretResult run() {
   }
 
   #undef READ_BYTE
+  #undef READ_SHORT
   #undef READ_CONSTANT
   #undef READ_STRING
   #undef BINARY_OP
